b98aec33fc1cd26087772ed1e451fdc53bb81bfc
Madejczyk <jakub.madejczyk92@gmail.com>
2019-11-15 19:26:32 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <h1>
+    Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello,
+    world!Hello, world!Hello, world!Hello, world!Hello, world!
+  </h1>
+);
+
+export default App;
