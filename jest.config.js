d88340386b6698648226f52d9cd475246dae8b9c
Madejczyk <jakub.madejczyk92@gmail.com>
2019-11-16 19:32:11 +0100

    Modify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: "jest-puppeteer",
     globals: {
-      URL: "http://localhost:8080"
+      URL: process.env.IP ? `http://${process.env.IP}:${process.env.PORT}` : "http://localhost:8080"
     },
     testMatch: [
       "**/test/**/*.test.js"
