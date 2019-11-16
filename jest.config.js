b1c03278ef1419a1fb68988aa26a1f759a9e667a
Madejczyk <jakub.madejczyk92@gmail.com>
2019-11-16 19:04:07 +0100

    change port

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: "jest-puppeteer",
     globals: {
-      URL: "http://localhost:9000"
+      URL: "http://localhost:8080"
     },
     testMatch: [
       "**/test/**/*.test.js"
