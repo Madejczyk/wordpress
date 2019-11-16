ad3942a27ff05b27f0afbf713c437c91ab687495
Madejczyk <jakub.madejczyk92@gmail.com>
2019-11-16 17:10:58 +0100

    Run server on circleCI

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const express = require("express")
 const app = express()
-const port = 3000
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.set("port", process.env.PORT || 8080);
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+const PORT = process.env.PORT || 5000
+express()
+    .use(express.static(path.join(__dirname, "dist")));
+    .listen(PORT, () => console.log(`Listening on port ${PORT}!`))
