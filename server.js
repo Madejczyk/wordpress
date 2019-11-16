9f6de595bf9da35a2f6c82b956b885e1599d39fd
Madejczyk <jakub.madejczyk92@gmail.com>
2019-11-16 17:40:25 +0100

    Return index.html

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-    .use(express.static(path.join(__dirname, "dist")))
-    .listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+app.use(express.static(path.join(__dirname, "dist")))
+
+app.get("/", function(req, res){
+    res.render("index.html");
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
